dc22f9c24afddeffa6509dd10681acd6de2de6b6
dfinke <finked@hotmail.com>
2023-02-03 10:49:33 -0500

    support pipelining

--- Public/ConvertFrom-GPTMarkdownTable.ps1
+++ Public/ConvertFrom-GPTMarkdownTable.ps1
@@ -1,15 +1,36 @@
 function ConvertFrom-GPTMarkdownTable {
+    <#
+    .SYNOPSIS
+        Converts a markdown table to a PowerShell object.
+
+    .PARAMETER Markdown
+        The markdown table to convert.
+    .EXAMPLE
+        ConvertFrom-GPTMarkdownTable -Markdown @'
+        | Name | Value |
+        | ---- | ----- |
+        | foo  | bar   |
+        | baz  | qux   |
+        '@
+
+    .EXAMPLE
+        ai 'markdown table syntax' | ConvertFrom-GPTMarkdownTable        
+    #>
     param(
+        [Parameter(ValueFromPipeline)]
         $markdown
     )
- 
-    $lines = $markdown.Trim() -split "`n"
 
-    $(
-        foreach ($line in $lines) {
-            if ($line -match '[A-Za-z0-9]') {
-                $line.Trim() -replace "^\|", ""
+    End {
+        
+        $lines = $markdown.Trim() -split "`n"
+
+        $(
+            foreach ($line in $lines) {
+                if ($line -match '[A-Za-z0-9]') {
+                    $line.Trim() -replace "^\|", ""
+                }
             }
-        }
-    ) | ConvertFrom-Csv -Delimiter '|'
+        ) | ConvertFrom-Csv -Delimiter '|'
+    }
 }
