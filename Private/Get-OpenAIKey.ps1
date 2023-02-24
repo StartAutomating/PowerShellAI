f23a67f757c1610e83efeb1c5de850237ba98206
Kris Borowinski <kris.borowinski@outlook.com>
2023-02-24 15:37:25 +0100

    Replace tabs with spaces

--- Private/Get-OpenAIKey.ps1
+++ Private/Get-OpenAIKey.ps1
@@ -1,5 +1,5 @@
 function Get-OpenAIKey {
-	if ($null -ne $Script:OpenAIKey) {
+    if ($null -ne $Script:OpenAIKey) {
         ConvertFrom-SecureString -SecureString $Script:OpenAIKey -AsPlainText
     } else {
         $env:OpenAIKey
