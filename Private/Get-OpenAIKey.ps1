3d85603da120fb5af313ffb0580b2fa3057f8444
Kris Borowinski <kris.borowinski@outlook.com>
2023-02-24 20:00:30 +0100

    Fix exception on PowerShell 6 and lower; Add help

--- Private/Get-OpenAIKey.ps1
+++ Private/Get-OpenAIKey.ps1
@@ -1,6 +1,21 @@
 function Get-OpenAIKey {
+    <#
+        .SYNOPSIS
+        Gets the OpenAIKey module scope variable or environment variable is set.
+
+        .EXAMPLE
+        Get-OpenAIKey
+    #>
     if ($null -ne $Script:OpenAIKey) {
-        ConvertFrom-SecureString -SecureString $Script:OpenAIKey -AsPlainText
+        if ($PSVersionTable.PSVersion.Major -gt 6) {
+            #On PowerShell 7 and higher use AsPlainText parameter
+            ConvertFrom-SecureString -SecureString $Script:OpenAIKey -AsPlainText
+        } else {
+            #On PowerShell 6 and lower use .NET marshaling
+            [Runtime.InteropServices.Marshal]::PtrToStringAuto(
+                [Runtime.InteropServices.Marshal]::SecureStringToBSTR($Script:OpenAIKey)
+            )
+        }
     } else {
         $env:OpenAIKey
     }
