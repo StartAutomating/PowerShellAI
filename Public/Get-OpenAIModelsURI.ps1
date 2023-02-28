19249b0c4e69a4958da3f9fd2ac9cae3892ea94b
James Brundage <@github.com>
2023-02-27 19:56:50 -0800

    Get-OpenAIModelsURI: Minor Help Fixes (Fixes #56)

--- Public/Get-OpenAIModelsURI.ps1
+++ Public/Get-OpenAIModelsURI.ps1
@@ -2,6 +2,12 @@ function Get-OpenAIModelsURI {
     <#
         .Synopsis
         Base url for OpenAI Models API
+        .DESCRIPTION
+        Gets the base URI for the OpenAI models API.
+        .EXAMPLE
+        Get-OpenAIModelsURI
+        .LINK
+        https://platform.openai.com/docs/models/overview
     #>
     
     (Get-OpenAIBaseRestURI) + '/models'
