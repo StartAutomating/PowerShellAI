16dae14a95a8602224d4ca3ceb0968ff3ad4ea7a
James Brundage <@github.com>
2023-02-27 19:56:04 -0800

    Get-OpenAIEditsURI: Minor Help Fixes (Fixes #56)

--- Public/Get-OpenAIEditsURI.ps1
+++ Public/Get-OpenAIEditsURI.ps1
@@ -2,6 +2,12 @@ function Get-OpenAIEditsURI {
     <#
         .Synopsis
         Base url for OpenAI Edits API
+        .DESCRIPTION
+        Gets the base URI for the OpenAI Edits.
+        .EXAMPLE
+        Get-OpenAIEditsURI
+        .LINK
+        Get-OpenAIBaseRestURI
     #>
     (Get-OpenAIBaseRestURI) + '/edits'
 }
