45c6da0aaf0118f9a3e7a9e68024b0490fc1857b
James Brundage <@github.com>
2023-02-27 19:56:24 -0800

    Get-OpenAIImageGenerationsURI: Minor Help Fixes (Fixes #56)

--- Public/Get-OpenAIImagesGenerationsURI.ps1
+++ Public/Get-OpenAIImagesGenerationsURI.ps1
@@ -3,6 +3,12 @@ function Get-OpenAIImagesGenerationsURI {
     <#
         .Synopsis
         Base url for OpenAI Images Generations API
+        .DESCRIPTION
+        Gets the base URI for the OpenAI image generations.
+        .EXAMPLE
+        Get-OpenAIImagesGenerationsURI
+        .LINK
+        Get-OpenAIBaseRestURI
     #>
     
     (Get-OpenAIBaseRestURI) + '/images/generations'
