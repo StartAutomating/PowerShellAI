7de0f68810613ea638fda04a3c8434039741bb19
James Brundage <@github.com>
2023-02-27 19:55:42 -0800

    Get-OpenAICompletionsURI: Minor Help Fixes (Fixes #56)

--- Public/Get-OpenAICompletionsURI.ps1
+++ Public/Get-OpenAICompletionsURI.ps1
@@ -2,6 +2,12 @@ function Get-OpenAICompletionsURI {
     <#
         .Synopsis
         Base url for OpenAI Completions API
+        .Description
+        Gets the base URI for the OpenAI Completions.
+        .EXAMPLE
+        Get-OpenAICompletionsURI
+        .LINK
+        Get-OpenAIBaseRestURI   
     #>
     
     (Get-OpenAIBaseRestURI) + '/completions'
