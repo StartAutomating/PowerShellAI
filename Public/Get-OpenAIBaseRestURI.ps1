8df9df305e6c813dedac3d2891ef93f1b17dcf7d
James Brundage <@github.com>
2023-02-27 19:55:21 -0800

    Get-OpenAIBaseRestURI: Minor Help Fixes (Fixes #56)

--- Public/Get-OpenAIBaseRestURI.ps1
+++ Public/Get-OpenAIBaseRestURI.ps1
@@ -2,9 +2,12 @@ function Get-OpenAIBaseRestURI {
     <#
         .Synopsis
         Base url for OpenAIBase API
-        
+        .DESCRIPTION
+        Gets the base URI used for all OpenAI API requests.        
         .Example
-        Invoke-OpenAIAPI ((Get-GHBaseRestURI)+'/models')
+        Invoke-OpenAIAPI ((Get-OpenAIBaseRestURI)+'/models')
+        .LINK
+        Invoke-OpenAIAPI
     #>
     
     'https://api.openai.com/v1'
