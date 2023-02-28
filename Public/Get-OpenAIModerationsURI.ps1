0c674ab819c0ad5d8c500bf5804fd8d5a363d6a2
James Brundage <@github.com>
2023-02-27 19:57:38 -0800

    Get-OpenAIModerationsURI: Minor Help Fixes (Fixes #56)

--- Public/Get-OpenAIModerationsURI.ps1
+++ Public/Get-OpenAIModerationsURI.ps1
@@ -2,6 +2,10 @@ function Get-OpenAIModerationsURI {
     <#
         .Synopsis
         Base url for OpenAI Moderations API
+        .DESCRIPTION
+        Gets the base URI for the OpenAI moderations API.
+        .EXAMPLE
+        Get-OpenAIModelsURI
     #>
     
     (Get-OpenAIBaseRestURI) + '/moderations'
