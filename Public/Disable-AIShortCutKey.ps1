5ed15183723cbd7b97f13fd81cf1febe5eb4ce7b
James Brundage <@github.com>
2023-02-27 19:53:45 -0800

    Disable-AIShortCutKey: Minor Help Fixes (Fixes #56)

--- Public/Disable-AIShortCutKey.ps1
+++ Public/Disable-AIShortCutKey.ps1
@@ -2,7 +2,10 @@ function Disable-AIShortCutKey {
     <#
         .SYNOPSIS
         Disable the Ctrl+g shortcut key for getting completions
-
+        .DESCRIPTION
+        Disables CTRL+G as the shortcut key for completing code.
+        .LINK
+        Enable-AIShortcutKey
         .EXAMPLE
         Disable-AIShortCutKey
     #>
