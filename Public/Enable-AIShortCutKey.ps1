e251a0da0a80257bc5458e61737dba47675b22d7
James Brundage <@github.com>
2023-02-28 16:43:01 -0800

    Enable-AIShortcutKey: Adding -ShortcutKey and defaulting to 'ALT+G' in VSCode (Fixes #58)

--- Public/Enable-AIShortCutKey.ps1
+++ Public/Enable-AIShortCutKey.ps1
@@ -1,12 +1,33 @@
 function Enable-AIShortCutKey {
     <#
         .SYNOPSIS
-        Enable the Ctrl+g shortcut key for getting completions
+            Enable a shortcut key for getting completions
+        .DESCRIPTION
+            Enables a shortcut key for getting completions.
 
+            The -ShortcutKey can be customized.
+
+            If not in Visual Studio Code, the ShortcutKey will default to 'CTRL+G'
+            In Visual Studio Code, the ShortcutKey will default to 'ALT+G'
         .EXAMPLE
-        Enable-AIShortCutKey
+            # Enables the shortcut key.  Outside of VSCode, CTRL+G.  Inside of VSCode, ALT+G.
+            Enable-AIShortCutKey
+        .EXAMPLE
+            Enable-AIShortCutKey -ShortcutKey "CTRL+ALT+P"
     #>
-    Set-PSReadLineKeyHandler -Key Ctrl+g `
+    param(
+    [string]
+    $ShortcutKey = $(
+        # In Visual Studio Code, CTRL+G is "goto",
+        if ((Get-Process -id $pid).Parent.ProcessName -eq 'code') {
+            "ALT+G" # so we'll use ALT+G by default.
+        } else {
+            "CTRL+G" # If we're not running in code, use CTRL+G by default
+        }
+    )
+    )
+
+    Set-PSReadLineKeyHandler -Key $ShortcutKey `
         -BriefDescription OpenAICli `
         -LongDescription "Calls Open AI on the current buffer" `
         -ScriptBlock {
